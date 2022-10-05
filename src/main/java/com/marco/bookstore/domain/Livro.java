3b249ec29dd5153b1036070ed11ece0501e32d19
marcoantoniosilvapegado <marcoantoniosilvapegado@gmail.com>
2022-10-05 20:16:03 -0300

    Finalizando classes de dominio

--- src/main/java/com/marco/bookstore/domain/Livro.java
+++ src/main/java/com/marco/bookstore/domain/Livro.java
@@ -1,7 +1,24 @@
 package com.marco.bookstore.domain;
 
-public class Livro {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Livro implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String titulo;
@@ -10,6 +27,8 @@ public class Livro {
 
 	private String texto;
 
+	@ManyToOne
+	@JoinColumn(name="categoria_id")
 	private Categoria categoria;
 
 	public Livro(Integer id, String titulo, String nome_autor, String texto, Categoria categoria) {
