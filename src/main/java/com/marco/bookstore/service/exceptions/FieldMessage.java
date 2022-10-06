8c8e9f528d3eb7b34a1fd3da3f9123799083d216
marcoantoniosilvapegado <marcoantoniosilvapegado@gmail.com>
2022-10-06 19:32:11 -0300

    ajustes

---
+++ src/main/java/com/marco/bookstore/service/exceptions/FieldMessage.java
@@ -0,0 +1,43 @@
+package com.marco.bookstore.service.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	
+	private String message;
+
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public FieldMessage() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}		
+
+}
